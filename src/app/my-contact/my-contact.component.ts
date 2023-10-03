37281a08693bce54c406b8f7ea4cfc284bb3628f
dijithdamodaran <dijithdamodaran@gmail.com>
2023-10-03 10:34:20 +0530

    this is my first angular project create component

---
+++ src/app/my-contact/my-contact.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-contact',
+  templateUrl: './my-contact.component.html',
+  styleUrls: ['./my-contact.component.css']
+})
+export class MyContactComponent {
+
+
+}
---
+++ src/app/my-home/my-home.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-home',
+  templateUrl: './my-home.component.html',
+  styleUrls: ['./my-home.component.css']
+})
+export class MyHomeComponent {
+
+
+}
